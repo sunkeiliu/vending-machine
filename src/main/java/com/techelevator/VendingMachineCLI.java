c3061322098f14708a633705070371b9729e5c8f
Yoav Morahg <yoav@techelevator.com>
2023-06-08 10:34:56 -0400

    code push

---
+++ src/main/java/com/techelevator/VendingMachineCLI.java
@@ -0,0 +1,25 @@
+package com.techelevator;
+
+import java.util.Scanner;
+
+public class VendingMachineCLI {
+
+	// Use this for keyboard input - it is initialized in the constructor
+	private Scanner userInput;
+
+	public VendingMachineCLI(Scanner userInput) {
+		this.userInput = userInput;
+	}
+
+	public void run() {
+
+			// Add a loop here for your menu
+
+	}
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		VendingMachineCLI cli = new VendingMachineCLI(input);
+		cli.run();
+	}
+}
